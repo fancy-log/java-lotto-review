e6875aea182ba370f652e5b06d9078d05a6a0f0f
junyong <jymoon1121@gmail.com>
2022-12-10 22:38:11 +0900

    feat(Validate) : 로또 번호를 입력할 때 잘못된 번호를 입력하는 경우 예외 처리

--- src/main/java/lotto/util/Validate.java
+++ src/main/java/lotto/util/Validate.java
@@ -1,19 +1,66 @@
 package lotto.util;
 
+import static lotto.util.ExceptionMessage.*;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Validate {
-    private static final String ERROR_MESSAGE = "[ERROR] : ";
-    private static final int ZERO = 0;
+
     public int checkInputMoney(String input) {
         if (!input.matches("[0-9]+")) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + "숫자만 입력 가능합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + ERROR_ONLY_NUMBER);
         }
         int money = Integer.parseInt(input);
         if(money % 1000 != ZERO) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + "1,000원 단위로 입력만 가능합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + ERROR_LOTTO_PRICE);
 
         }
         return money;
+    }
+    public void checkWinningNumberInfo(List<String> numbers) {
+        checkWinningNumberSize(numbers);
+        checkWinningNumberNoDuplicate(numbers);
+        checkWinningNumberRange(numbers);
+    }
+
+    private void checkWinningNumberSize(List<String> numbers) {
+        if(numbers.size() != WINNING_NUMBER_COUNT) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + ERROR_WINNING_NUMBER_COUNT);
+        }
+    }
+
+    private void checkWinningNumberNoDuplicate(List<String> numbers) {
+        Set<String> numbersNoDuplicate = new HashSet<>(numbers);
+        if(numbersNoDuplicate.size() != WINNING_NUMBER_COUNT) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + ERROR_WINNING_NUMBER_NO_DUPLICATE);
+        }
+    }
+
+    private void checkWinningNumberRange(List<String> numbers) {
+        for(int i = 0; i < numbers.size(); i++) {
+            String number = numbers.get(i);
+            if(!number.matches("^[0-9]*$")) {
+                throw new IllegalArgumentException(ERROR_MESSAGE + ERROR_ONLY_NUMBER);
+            }
+            int num = Integer.parseInt(numbers.get(i));
+            if(num < LOTTO_NUMBER_RANGE_MIN || num > LOTTO_NUMBER_RANGE_MAX) {
+                throw new IllegalArgumentException(ERROR_MESSAGE + ERROR_LOTTO_NUMBER_RANGE);
+            }
+        }
+    }
+
+    public void checkBonusNumberInfo(String bonusInput, List<Integer> winningNumber) {
+        if(!bonusInput.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + ERROR_ONLY_NUMBER);
+        }
+        int bonusNumber = Integer.parseInt(bonusInput);
+        if(winningNumber.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + ERROR_NUMBER_NO_DUPLICATE);
+        }
+        if(bonusNumber < LOTTO_NUMBER_RANGE_MIN || bonusNumber > LOTTO_NUMBER_RANGE_MAX) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + ERROR_LOTTO_NUMBER_RANGE);
+        }
     }
 }
