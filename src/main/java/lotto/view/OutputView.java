3dbc34c51dde93c8de9164d7f7b6ef8a83b7fb9a
junyong <jymoon1121@gmail.com>
2022-12-11 01:52:05 +0900

    feat(OutputView) : 당첨내역 및 수익률 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,7 +2,7 @@ package lotto.view;
 
 import java.util.List;
 
-import lotto.domain.Lotto;
+import lotto.model.Lotto;
 
 public class OutputView {
     public static void printLottoMoneyCount(List<Lotto> lottos) {
@@ -10,8 +10,19 @@ public class OutputView {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
         }
+        System.out.println();
     }
     public static void printEnter() {
         System.out.println();
+    }
+    public static void printResult(int revenue, int [] countRank, int paid) {
+        String rate = String.format("%.1f", (double) revenue * 100 / paid);
+        System.out.println("당첨 통계\n---");
+        System.out.println("3개 일치 (5,000원) - " + countRank[5] + "개");
+        System.out.println("4개 일치 (50,000원) - " + countRank[4] + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + countRank[3] + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + countRank[2] + "개");
+        System.out.println("6개 일치) (2,000,000,000) - " + countRank[1] + "개");
+        System.out.println("총 수익률은 " + rate + "%입니다.");
     }
 }
